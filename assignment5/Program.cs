486470ace0befb3cf55f1ff858e4060a5a1e44a0
visualstudio <visualstudio@122-PC23>
2019-04-12 19:03:15 -0400

    TextAnalysisTools

--- assignment5/Program.cs
+++ assignment5/Program.cs
@@ -8,8 +8,31 @@ namespace assignment5
 {
     class Program
     {
-        static void Main(string[] args)
+      static void Main(string[] args)
         {
+            Program a = new Program();
+            Console.WriteLine("the longest word is: ");
+            Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for procesing bt an analytical Engine."));
+            
+        }
+        public MyFunctionA(string input)
+        {
+            string[] words = input.Split('');
+            int wordArrayLength = new int[wordArrayLength];
+
+            foreach (var word in words)
+            {
+
+            }
+            string currentWord = words[0];
+            string nextWord;
+            for (int y = 0; y < words.Length -1; y++)
+            {
+                currentWord = words[y];
+                nextWord = words[y + 1];
+
+            }
+
         }
     }
 }
